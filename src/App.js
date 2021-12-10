01a404a3f6c543fb6810e6b93297d554c92f697c
minuantony <minuantony19@gmail.com>
2021-12-10 12:31:31 -0600

    Made few modifications

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App container-fluid">
-      <div>
-        <div className="row">
-          <header className="App-header border">
-            <h1>DICTIONARY</h1>
-            <form>
-              <input
-                type="text"
-                placeholder="Search a word"
-                className="col-6"
-              />
-              <input type="submit" value="Search" className="col-2" />
-            </form>
-            <br />
-          </header>
-        </div>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <h1>Dictionary App</h1>
+      </header>
     </div>
   );
 }
-
-export default App;
