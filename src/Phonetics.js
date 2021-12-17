bc534b3647743827a8ae76188b5e38a03a9a0465
minuantony <minuantony19@gmail.com>
2021-12-16 19:35:54 -0600

    Fixed build error in netlify
     Line 7:7:  Using target="_blank" without rel="noreferrer" (which implies rel="noopener") is a security risk in older browsers: see https://mathiasbynens.github.io/rel-noopener/#recommendations  react/jsx-no-target-blank

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -5,7 +5,7 @@ export default function Phonetics(props) {
   console.log(props.phonetic);
   return (
     <div className="phonetics">
-      <a href={props.phonetic.audio} target="_blank">
+      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
         {" "}
         Listen{" "}
       </a>
